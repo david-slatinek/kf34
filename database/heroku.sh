31fa1fb56b1899266491274c04fd812f5ab91058
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2021-08-18 13:35:37 +0200

    Update db.sql and upload script

--- database/heroku.sh
+++ database/heroku.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #heroku container:login
-#heroku create kf34-db --region eu
-#heroku addons:create --app kf34-db heroku-postgresql:hobby-dev
-#heroku pg:psql --app kf34-db <db.sql
-heroku pg:psql -a kf34-db
+#heroku create kf34
+#heroku addons:create --app kf34 heroku-postgresql:hobby-dev
+#heroku pg:psql --app kf34 <db.sql
+#heroku pg:psql -a kf34
+#SET TIMEZONE TO 'Europe/Ljubljana';
