749f5c5e28ea8e8d23e07d0cc710aab5b17d1eb0
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2021-08-24 13:21:38 +0200

    Add email notification for error handling

--- raspberry-pi/temperature_humidity.py
+++ raspberry-pi/temperature_humidity.py
@@ -3,10 +3,17 @@
 import adafruit_dht
 from board import D17
 import logging
-import os
+import subprocess
 
 logging.basicConfig(level=logging.ERROR, filename="errors.log", filemode="a", format="%(asctime)s---%(message)s",
                     datefmt="%d.%m.%Y %H:%M:%S")
+
+
+def handle_error(err):
+    logging.error(err)
+    subprocess.Popen(["./led_buzzer.py", "-e"])
+    subprocess.Popen(["./email.sh", "DHT22", err])
+
 
 if __name__ == "__main__":
     dht = adafruit_dht.DHT22(D17, use_pulseio=False)
@@ -15,8 +22,7 @@ if __name__ == "__main__":
         temperature, humidity = dht.temperature, dht.humidity
         print(f"Temperature: {temperature}C, humidity: {humidity}%")
     except RuntimeError as error:
-        logging.error(error.args[0])
+        handle_error(error.args[0])
     except Exception as error:
-        logging.error(error.args[0])
+        handle_error(error.args[0])
         dht.exit()
-        os.system("./led.py -e &")
