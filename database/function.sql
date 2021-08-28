729201b9ef53974aac52526580a5ba59884c6d38
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2021-08-28 13:15:00 +0200

    Add SQL function for inserting new data

---
+++ database/function.sql
@@ -0,0 +1,19 @@
+CREATE FUNCTION add_new_data(value DECIMAL(5, 2), d_type VARCHAR(50))
+RETURNS BOOLEAN AS $$ 
+DECLARE id_fk INT;
+
+BEGIN
+    SELECT id_device INTO id_fk
+    FROM device
+    WHERE device_type = d_type;
+    
+    IF FOUND THEN
+        INSERT INTO data(value, fk_device)
+        VALUES (value, id_fk);
+        RETURN TRUE;
+    END IF;
+RETURN FALSE;
+
+END;
+
+$$ LANGUAGE plpgsql;
--- database/heroku.sh
+++ database/heroku.sh
@@ -3,4 +3,5 @@
 #heroku create kf34
 #heroku addons:create --app kf34 heroku-postgresql:hobby-dev
 #heroku pg:psql --app kf34 <db.sql
+#heroku pg:psql -a kf34 <function.sql
 #heroku pg:psql -a kf34
