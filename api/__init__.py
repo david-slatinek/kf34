aeb919f045ae2614cdd4d86ca29e4c9798d9d4dc
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2021-09-05 14:01:24 +0200

    Add environment variables file

--- api/__init__.py
+++ api/__init__.py
@@ -3,18 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from subprocess import run
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from config import key
+from os import environ
+from dotenv import load_dotenv
 
-raw_db_url = run(
-    ["heroku", "config:get", "DATABASE_URL", "-a", "kf34"],
-    capture_output=True).stdout
+load_dotenv()
+
+raw_db_url = run(["heroku", "config:get", "DATABASE_URL", "-a", "kf34"], capture_output=True).stdout
 db_url = raw_db_url.decode("ascii").strip()
 db_url = "postgresql:" + db_url.split(":", 1)[1]
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["KEY"] = key
+app.config["KEY"] = environ.get("KEY")
 db = SQLAlchemy(app)
 
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
