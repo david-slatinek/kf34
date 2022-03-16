0fc53be39fe98f537af6e3bb5cf725804e706901
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-03-16 16:23:04 +0100

    Delete status.py

--- raspberry-pi/status.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-if __name__ == "__main__":
-    try:
-        with open("status.txt") as f:
-            print(f"Status: {f.readline()}", end="")
-    except IOError as error:
-        print(error)
