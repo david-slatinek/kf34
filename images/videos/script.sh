0bcc34528e2933c81f9d845327edc7b96454951c
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-05-05 13:53:32 +0200

    Move videos, update script.sh, add compression for video

---
+++ images/videos/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#tar -czvf video.tar.gz input-video.mp4
+
+tar -xzvf video.tar.gz
+
+input="input-video.mp4"
+output="app.gif"
+name="palette.png"
+
+filters="fps=15,scale=300:-1:flags=lanczos"
+ffmpeg -v verbose -i $input -vf palettegen $name
+ffmpeg -v verbose -i $input -vf "$filters,palettegen" -y $name
+ffmpeg -v verbose -i $input -i $name -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output
+
+rm $name $input
