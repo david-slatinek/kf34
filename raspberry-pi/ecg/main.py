9ec537473c1bc5b42d55594492f78d15b30847a6
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-05-27 16:03:20 +0200

    Update ecg main.py

--- raspberry-pi/ecg/main.py
+++ raspberry-pi/ecg/main.py
@@ -6,14 +6,16 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import matplotlib.pyplot as plt
 import collections
+from adafruit_ads1x15.ads1115 import Mode
 
 i2c = busio.I2C(SCL, SDA)
 ads = ADS.ADS1115(i2c)
+ads.mode = Mode.CONTINUOUS
 channel = AnalogIn(ads, ADS.P0)
 
 
 if __name__ == "__main__":
-    data = collections.deque(0 for _ in range(10))
+    data = collections.deque(0 for _ in range(20))
 
     while True:
         try:
@@ -22,6 +24,8 @@ if __name__ == "__main__":
             data.append(channel.value)
 
             plt.cla()
+            plt.yticks([])
+            plt.xticks([])
             plt.title("ECG")
             plt.plot(data)
             plt.pause(0.01)
